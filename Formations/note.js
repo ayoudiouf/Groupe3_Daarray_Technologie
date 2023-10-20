a70fa240d93d2d1c0233cfdb21feee854f5db979
saloum45 <saloumfall45@gmail.com>
2023-10-20 10:34:54 +0000

    rating system in progress

--- Formations/note.js
+++ Formations/note.js
@@ -2,11 +2,15 @@
 let stars = document.querySelectorAll("#star");
 
 function starPosition(id) {
+  let note={
+    
+  }
   stars.forEach((star) => {
     star.setAttribute("class", "bi bi-star");
   });
-  // alert('etoile '+id+" cliqué");
+  // alert('etoile '+id+" cliquée");
   for (let i = 0; i < id; i++) {
     stars[i].setAttribute("class", "bi bi-star-fill");
   }
+
 }
